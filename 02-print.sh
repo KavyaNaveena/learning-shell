22522c25ca73ce7f71c97797070c5c617f4a1371
Kavya Naveena <karumurikavya@gmail.com>
2023-02-25 17:06:26 +0530

    executing colors in shell

--- 02-print.sh
+++ 02-print.sh
@@ -1,1 +1,17 @@
 echo helloworld
+
+## Color Syntax
+# echo -e "\e[COLm Message \e[0m"
+# echo -e ->Enable colors from echo command
+# \e[COLm ->Enable particular color from echo
+# Color     codes
+# Red          31
+# Green        32
+# Yellow       33
+# Blue         34
+# Magenta      35
+# Cyan         36
+
+# \e[0m  -> to disable color, Zero code is going to reset the color, meaning if we enable color for sure we need to disable it, otherwise color will continue on scrren.
+
+echo -e "\e[35m Hello Shell Scripting \e[0m"
